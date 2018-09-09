231776baf07bb24f892c9c5e261c9d0a172ed5c4
yogesh <patilyogesh491@gmail.com>
2018-09-09 19:52:55 +0530

    New Message

---
+++ src/test/java/com/myntra/testframework/utility/Helper.java
@@ -0,0 +1,32 @@
+package com.myntra.testframework.utility;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.omg.CORBA.Current;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Helper {
+	
+	
+	public static String getScreenshot(WebDriver driver, String path) throws IOException
+	{
+		TakesScreenshot st=(TakesScreenshot) driver;
+		
+		File scr = st.getScreenshotAs(OutputType.FILE);
+		
+		String dest="C:\\Users\\Yogesh\\git\\NewProject\\com.myntra.project\\Screenshots\\"+path+".png";
+		
+		org.openqa.selenium.io.FileHandler.copy(scr, new File(dest));
+		
+		return dest;
+	
+	}
+	
+
+	
+	
+	
+}
